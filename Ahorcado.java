baeeaf94c4fd573d56dbdc5f42b7ceb6906d642b
MarioAndres75 <102887327+MarioAndres75@users.noreply.github.com>
2022-04-04 21:08:21 -0300

    Update Ahorcado.java

--- Ahorcado.java
+++ Ahorcado.java
@@ -1,4 +1,5 @@
 package tp1;
+// modificacion numero uno
 import java.util.Scanner;
 public class Ahorcado {
  public static void main(String[] args) {
@@ -7,7 +8,7 @@ public class Ahorcado {
  Integer opcion;
  Integer resultado;
  do{
- System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+ System.out.println("Ingrese su operaciÃ³n /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
  opcion = teclado.next();
  }while(opcion < 1 && opcion > 4)
  System.out.println("Ingrese el primer numero");
